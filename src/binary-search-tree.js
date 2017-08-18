35fb7e6b8f78e0d62616310c5d684edd9827cb7f
Tyler <blackbelt@yahoo.com>
2017-08-18 19:15:13 -0400

    worked on binary search tree, did not quite finish it

--- src/binary-search-tree.js
+++ src/binary-search-tree.js
@@ -1,10 +1,50 @@
 // https://msdn.microsoft.com/en-us/library/aa289150(v=vs.71).aspx
 class BinarySearchTree {
-  constructor(value) {
-    this.value = value;
+  constructor(val) {
+    this.value = val;
     this.left = null;
-    this.right = null;
+    this.right = null;  
+
+    function insert(val) {
+      if ( (val < this.value) && (this.left) ) {
+        this.left.insert(val);
+      }
+      if ( (val < this.value) && (!this.left) ) {
+        this.left = new BinarySearchTree(val);
+      }
+      if ( (val > this.value) && (this.right) ) {
+        this.right.insert(val);
+      }
+      if ( (val > this.value) && (!this.left) ) {
+        this.right = new BinarySearchTree(val);
+      }
+    }
+
+    function contains(searched) {
+      if (this.value === searched) {
+        return true;
+      }
+      return !!(this.left && this.left.contains(searched)) || !!(this.right && this.right.contains(searched));
+    }
+
+    function depthFirstForEach() {
+
+    }
+
+    function breadFirstForEach() {
+
+    }
   }
 }
 
 module.exports = BinarySearchTree;
+
+/*
+
+* Should have the methods: `insert`, `contains`, `depthFirstForEach`, and `breadthFirstForEach`.
+  * `insert(value)` inserts the new value at the correct location in the tree.
+  * `contains(value)` searches the tree and returns `true` if the the tree contains the specified value.
+  * `depthFirstForEach(cb)` should iterate over the tree using DFS and passes each node of the tree to the given callback function.
+  * `breadthFirstForEach(cb)` should iterate over the tree using BFS and passes each node of the tree to the given callback function (hint: you'll need to either re-implement or import a queue data structure for this).
+
+*/
